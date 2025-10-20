c8325f71446375ed8051fefe4cd0295a691bd930
Oleksandr Moiseienko <omoiseenko@dfusiontech.com>
2025-10-20 23:41:10 +0300

    feat(auth): Improve keyboard handling on login screen

--- pages/LoginScreen.tsx
+++ pages/LoginScreen.tsx
@@ -1,28 +1,38 @@
 import React, { memo } from 'react';
 
-import { StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
+import { StyleSheet, Platform } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 import AuthProcess from '@/processes/auth/ui/AuthProcess';
 import { colors, theme } from '@/shared/config';
 
+const scrollResetCoords = { x: 0, y: 0 };
+
 const LoginScreen = () => {
   return (
-    <KeyboardAvoidingView
-      keyboardVerticalOffset={-100} // for iOS keyboard to decrease the distance between the keyboard and the input fields
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    <KeyboardAwareScrollView
       style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      resetScrollToCoords={scrollResetCoords}
+      keyboardShouldPersistTaps='handled'
+      enableOnAndroid={true}
+      enableAutomaticScroll={Platform.OS === 'ios'}
+      extraScrollHeight={20}
     >
       <AuthProcess />
-    </KeyboardAvoidingView>
+    </KeyboardAwareScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
     backgroundColor: colors.background,
     flex: 1,
+  },
+  contentContainer: {
+    flexGrow: 1,
     justifyContent: 'center',
+    marginTop: -50, // push the form up a bit
     paddingHorizontal: theme.spacing.m,
   },
 });
